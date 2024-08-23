15aae0902ccacd822af0b209c8e5e9c6da39b02f
fxckat36 <160757035+fxckat36@users.noreply.github.com>
2024-08-23 06:27:29 +0300

    Update urls.py

--- LaHrust/urls.py
+++ LaHrust/urls.py
@@ -4,6 +4,6 @@ from django.urls import path, re_path
 from api import views as api_views
 from user_view import views as user_views
 urlpatterns = [
-    re_path(r'^main', user_views.main),
+    path('https://fxckat36.github.io/LaHrust', user_views.main),
     path('admin/', admin.site.urls),
 ]
